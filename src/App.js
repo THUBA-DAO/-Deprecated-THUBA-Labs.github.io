c46ada44411d2fb2f9387ffe6517aa971d9b1856
miracleXH <h1047741619@163.com>
2021-09-21 13:24:16 +0800

    add pic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import bg from './assets/head_bg.svg';
+import bg_pic from './assets/bg_pic.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +18,16 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
-        hello,world!
-
-      </header>
+        </a>
+        <p style={{ fontSize: 800 }}>12345678</p>
+      </header> */}
+      <div>
+        <img src={bg} className="head_bg1" alt=""  />
+        <img src={bg_pic} className="head_bg2" alt=""  />
+        <div className="head_words">
+        <div className="head_font">THUBA旨在构建学生和区块链行业之间的链接桥梁</div>
+      </div>
+      </div>
     </div>
   );
 }
