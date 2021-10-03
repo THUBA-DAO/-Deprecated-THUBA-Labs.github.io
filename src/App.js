ffc4fe0bb82082ffb83217f0d9e8b499c7c4c075
miracleXH <h1047741619@163.com>
2021-10-03 22:28:03 +0800

    add sth

--- src/App.js
+++ src/App.js
@@ -30,50 +30,60 @@ import mail_box from './assets/mail_box.svg'
 import public_wechat from './assets/public-we.png'
 import wechat_helper from './assets/wechat-helper.png'
 import logo_colored from './assets/logo_colored.svg'
+import React, { Component } from 'react'
 
-function App() {
+class App extends Component {
+  scrollToAnchor = (anchorName) => {
+    if (anchorName) {
+        // 找到锚点
+        let anchorElement = document.getElementById(anchorName);
+        // 如果对应id的锚点存在，就跳转到锚点
+        if(anchorElement) { anchorElement.scrollIntoView(); }
+    }
+  }
+  render() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p style={{ fontSize: 800 }}>12345678</p>
-      </header> */}
       <div>
+        <a id="top"></a>
         <img src={bg} className="head_bg1" alt=""  />
         <img src={bg_pic} className="head_bg2" alt=""  />
         <div className="head_words">
-          <div className="head_font">THUBA旨在构建学生和区块链行业之间的链接桥梁。</div>
+          <div className="head_font">THUBA旨在构建学生和区块链行业之间的链接桥梁</div>
         </div>
         <img src={logo} className="logo" alt=""/>
-        <div className="header_font header_button_1">首页</div>
-        <div className="header_font header_button_2">资源</div>
+        <a onClick={()=>this.scrollToAnchor('top')} className="btn">
+          <div className="header_font header_button_1">首页</div>
+        </a>
+        <a onClick={()=>this.scrollToAnchor('resources')} className="btn">
+          <div className="header_font header_button_2">资源</div>
+        </a>
+        <a onClick={()=>this.scrollToAnchor('team')} className="btn">
         <div className="header_font header_button_3">团队</div>
-        <div className="header_font header_button_4">合作</div>
-        <div className="header_font header_button_5">联系</div>
+        </a>
+        <a onClick={()=>this.scrollToAnchor('cooperate')} className="btn">
+          <div className="header_font header_button_4">合作</div>
+        </a>
+        <a onClick={()=>this.scrollToAnchor('contact')} className="btn">
+          <div className="header_font header_button_5">联系</div>
+        </a>
         <div className="header_font header_button_6">EN/中文</div>
       </div>
-
-      <div className="subtitle">精彩回顾</div>
+      
+        <div className="subtitle"><a id="resources">精彩回顾</a></div>
+      
       <div>        
         <img src={course_pic} className="course_pic" alt=""/>
         <div className="course_card_pos card">
           <div className="course_card_title_pos card_title">区块链课程</div>
           <div className="course_words card_font">系统性区块链课程，从入门到应用</div>
-          <div className="card_btn">
-            <div className="card_btn_words">点击观看</div>
-            <img src={course_arrow} className="card_arrow" alt=""/>
-          </div>
+          
+          <a href="https://space.bilibili.com/489099832/channel/seriesdetail?sid=294730" target="_blank" rel="noopener noreferrer">
+            <div className="card_btn">
+              <div className="card_btn_words">点击观看</div>          
+              <img src={course_arrow} className="card_arrow" alt=""/>
+            </div>
+          </a>
         </div>
       </div>
       <img src={arrow_yellow} className="arrow_yellow arrow_yellow_1" alt=""/>
@@ -82,10 +92,12 @@ function App() {
         <div className="talk_card_pos card">
           <div className="talk_card_title_pos card_title">THUBA Talk</div>
           <div className="talk_words card_font">回顾THUBA各类区块链活动</div>
+          <a href="https://space.bilibili.com/489099832/channel/seriesdetail?sid=294675" target="_blank" rel="noopener noreferrer">
           <div className="card_btn">
             <div className="card_btn_words">点击回顾</div>
             <img src={course_arrow} className="card_arrow"  alt=""/>
           </div>
+          </a>
         </div>
         <img src={talk_pic} className="talk_pic" alt=""/>
       </div>
@@ -97,13 +109,17 @@ function App() {
         <div className="hackathon_card_pos card">
           <div className="hackathon_card_title_pos card_title">参加黑客松</div>
           <div className=" card_font hackathon_words">参与THUBA黑客松平台，认识队友，获取奖金</div>
+          <a href="https://space.bilibili.com/489099832/channel/seriesdetail?sid=294689" target="_blank" rel="noopener noreferrer">
           <div className="card_btn">
             <div className="card_btn_words">视频展示</div>
             <img src={course_arrow} className="card_arrow" alt=""/>
           </div>
+          </a>
+          <a href="https://github.com/THUBA-Labs/Hackthons" target="_blank" rel="noopener noreferrer">
           <div className="card_btn_demo">
             <div className="card_btn_words_demo">demo源码</div>
           </div>
+          </a>
         </div>
       </div>
       <img src={arrow_rotate} className="arrow_yellow_3" alt=""/>
@@ -112,15 +128,17 @@ function App() {
         <div className="research_card_pos card">
           <div className="talk_card_title_pos card_title">区块链研究</div>
           <div className="talk_words card_font">获取前沿区块链学术研究与行业研究</div>
+          <a href="https://github.com/THUBA-Labs/Research" target="_blank" rel="noopener noreferrer">
           <div className="card_btn">
             <div className="card_btn_words">点击观看</div>
             <img src={course_arrow} className="card_arrow" alt=""/>
           </div>
+          </a>
         </div>
         <img src={research_pic} className="research_pic" alt=""/>        
       </div>
       
-      <div className="co-members">核心成员</div>
+      <div className="co-members"><a id="team">核心成员</a></div>
 
       <div>
         <div className="member-card member1">
@@ -184,7 +202,7 @@ function App() {
         </div>
       </div>
 
-      <div className="partners">合作伙伴</div>
+      <div className="partners"><a id="cooperate">合作伙伴</a></div>
       <div>
         <img src={p1} className="partners1" alt=""/>
         <img src={p2} className="partners2" alt=""/>
@@ -197,14 +215,17 @@ function App() {
       </div>
 
       <div className="contactus">
+        <a id="contact"></a>
         <div className="contact_title">联系我们</div>
         <img src={mail_box} className="mailbox" alt=""/>
         <div className="email">
           <div className="email_font">thublockchain@outlook.com</div>
         </div>
+        <a href="mailto:thublockchain@outlook.com">
         <div className="send_btn">
           <div className="send_font">Send</div>
         </div>
+        </a>
         <img src={public_wechat} alt="" className="public_pic"/>
         <div className="public">协会公众号</div>
         <img src={wechat_helper} alt="" className="helper_pic"/>
@@ -217,7 +238,12 @@ function App() {
 
 
     </div>
-  );
+  )
+  }
 }
+// function App() {
+  
+//   );
+// }
 
 export default App;
